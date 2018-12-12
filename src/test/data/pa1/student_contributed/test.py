83e877ce3ff248303182aadf803b5754fe8c37fa
Jireh <jireh@192.168.0.103>
2018-12-11 23:09:20 -0800

    Update test.py

--- src/test/data/pa1/student_contributed/test.py
+++ src/test/data/pa1/student_contributed/test.py
@@ -1,1 +1,14 @@
-
+x : int = 0
+y : int = 5
+z : Foo = None
+class Foo(object):
+    x : int = 3
+def foo(x : int) -> int:
+    y : str = "test"
+    def foo2() -> str:
+        nonlocal y
+        return y
+    nonlocal x
+    return x
+x = 1 and 2
+foo(y, 5)
